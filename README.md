0361efebc09708963e33f2430c7d8ebfb45bc57e
Marsan Ma <marsan@gmail.com>
2016-09-16 15:00:27 +0800

    [New] Initial commit.

---
+++ README.md
@@ -0,0 +1,76 @@
+# Twitter Scraper
+
+
+Scraping twitter content from [twitter streaming API](https://dev.twitter.com/streaming/overview), in python3.
+
+
+## How it works
+
+### Setup
+
+1. Copy the `config.yml.default` to `config.yml`, and fill your twitter application tokens you got from [twitter developers](https://apps.twitter.com/).
+
+
+2. Clone this repositiry and just type:  
+
+    python3 twitter.py
+    
+then the listener will start to dump corpus in: `corpus/<YYYYMMDD_HHMMSS.txt>`
+
+
+### Output
+
+I build this corpus for training the neural network model [**ChatBot**](https://github.com/Marsan-Ma/tf_chatbot_seq2seq_antilm), thus the corpus is arranged as consecutive dialog where even sentences are the response of odd sentences, like:
+
+
+Line number | Sentences
+------------|---------
+1 | happy birthday!
+2 | thank you :)
+3 | game of throne is the best drama i've seen.
+4 | I'll say the walking dead is even better.
+
+Where (1,2), (3,4) are two independent dialog pairs.
+
+
+## Content Filters
+
+Twitter streaming API supports [some filter](https://dev.twitter.com/streaming/overview/request-parameters), and the most useful  ones are:
+
+### Location filter
+simply place the logitude and altitude in [South,West,North,East] format, for example:
+
+Place | filter
+------|-------------
+San Francisco | [-122.75,36.8,-121.75,37.8])  
+New York City | [-74,40,-73,41]
+San Francisco or New York City | [-122.75,36.8,-121.75,37.8, -74,40,-73,41]
+
+
+### Language filter
+
+there are threads questioning about `language` filter is not work, actually is does, it just need to accompanied with `track` filter, like:
+
+
+This won't work.  
+`stream.filter(languages=["en"])`   
+
+This works.
+`stream.filter(languages=["en"], track=['machine', 'learning'])` 
+
+
+### If you want language other than English
+
+Twitter can't tokenize some languages like Japanese or Chinese correctly, your `track` parameter won't work for these languages. For example, you might setup:
+
+`stream.filter(languages=["ja"], track=['バイト'])`
+
+While you expect you will get a lot of tweets about **バイト**, but you simply can't get it, because twitter can't tokenize Japenses and Chinese correctly.
+
+If you just want some corpus, regardless of the topics, here are the **work around**: use some generic English keywords instead, like:
+
+`stream.filter(languages=["zh"], track=['I', 'you', 'http', 'www', '@', '。', '，', '！', '』', ')', '...', '-', '/'])`
+
+According to document, you could add up to 400 keywords on this list, even some emoji also works.
+
+
---
+++ twitter.py
@@ -0,0 +1,130 @@
+import os, sys, re, yaml, json
+import tweepy
+import time
+import socket
+import http.client
+
+from datetime import datetime
+from hanziconv import HanziConv
+
+from tweepy import OAuthHandler, Stream
+from tweepy.streaming import StreamListener
+
+
+# Number of seconds to wait after an exception before restarting the stream.
+tcpip_delay = 0.25
+MAX_TCPIP_TIMEOUT = 16
+
+
+class QueueListener(StreamListener):
+
+    def __init__(self):
+        """Creates a new stream listener with an internal queue for tweets."""
+        super(QueueListener, self).__init__()
+        self.num_handled = 0
+        self.queue = [] #Queue.Queue()
+        self.batch_size = 100
+
+        # tweepy
+        cfg = yaml.load(open('config.yml', 'rt'))['twitter']
+        self.auth = OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+        self.auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+        self.api = tweepy.API(self.auth)
+
+        # corpus file
+        if not os.path.exists('corpus'): os.makedirs('corpus')
+        self.dumpfile = "corpus/%s.txt" % datetime.now().strftime("%Y%m%d_%H%M%S")
+        
+
+    def on_data(self, data):
+        """Routes the raw stream data to the appropriate method."""
+        raw = json.loads(data)
+        if 'in_reply_to_status_id' in raw:
+            if self.on_status(raw) is False:
+                return False
+        elif 'limit' in raw:
+            if self.on_limit(raw['limit']['track']) is False:
+                return False
+        return True
+
+    def on_status(self, raw):
+        if isinstance(raw.get('in_reply_to_status_id'), int):
+            print("(%s)%s / %i" % (raw['in_reply_to_status_id'], raw['text'], len(self.queue)))
+            line = (raw.get('in_reply_to_status_id'), raw.get("text"))
+            self.queue.append(line)
+            if len(self.queue) >= self.batch_size: self.dump()
+        return True
+
+    def on_error(self, status):
+        print('ON ERROR:', status)
+
+    def on_limit(self, track):
+        print('ON LIMIT:', track)
+
+    def dump(self):
+        pcnt = 0
+        with open(self.dumpfile, 'a') as fdump:
+            (sids, texts), self.queue = zip(*self.queue), []
+            while True:
+                try:
+                    lines_mapper = {s.id_str: s.text for s in self.api.statuses_lookup(sids)}
+                    break
+                except Exception as e:
+                    print("[Error]", e)
+                    time.sleep(10)
+            lines_grps = [[lines_mapper.get(str(sid)), txt] for sid, txt in zip(sids, texts) if lines_mapper.get(str(sid))]
+            lines_grps = [[self.preprocess(s) for s in lines] for lines in lines_grps]
+            
+            for lines in lines_grps:
+                for i in range(len(lines)-1):
+                    fdump.write("%s\n%s\n" % (lines[i], lines[i+1]))
+                    pcnt += 1
+        self.num_handled += pcnt
+
+
+    is_zh = re.compile(r'([\p{IsHan}]+)', re.UNICODE)
+    def preprocess(self, line, cond=None):
+        line = HanziConv.toTraditional(line)
+        # line = re.sub(r"\@[a-z0-9][a-z0-9]*", '', line)
+        # line = re.sub(r"\#[a-z0-9][a-z0-9]*", '', line)
+        # line = re.split(r"\([a-z][a-z]\)", line.lower())[0]
+        if cond == 'only_zh':
+            words = [w for w in jieba.cut(line) if is_zh.search(w)]
+            line = ' '.join(words)
+        line = re.sub("\s+", ' ', line).strip().lower()
+        return line
+
+
+def main():
+    # open stream
+    listener = QueueListener()
+    stream = Stream(listener.auth, listener) #, language='zh')
+
+    # [stream filter]
+    stream.filter(locations=[-122.75,36.8,-121.75,37.8, -74,40,-73,41])  # San Francisco or New York City
+    # stream.filter(locations=[-122.75,36.8,-121.75,37.8])  # San Francisco
+    # stream.filter(locations=[-74,40,-73,41])  # New York City
+    # stream.filter(languages=["en"], track=['python', 'obama', 'trump'])
+    # stream.filter(languages=["zh"], track=['I', 'you', 'http', 'www', 'co', '@', '#', '。', '，', '！', '.', '!', ',', ':', '：', '』', ')', '...', '我', '你', '他', '哈', '的', '是', '人', '-', '/'])
+    # stream.filter(languages=["zh"], locations=[-180,-90,180,90])
+    # stream.filter(languages=["ja"], track=['バイト'])
+
+    try:
+        while True:
+            try:
+                stream.sample()  # blocking!
+            except KeyboardInterrupt:
+                print('KEYBOARD INTERRUPT')
+                return
+            except (socket.error, http.client.HTTPException):
+                global tcpip_delay
+                print('TCP/IP Error: Restarting after %.2f seconds.' % tcpip_delay)
+                time.sleep(min(tcpip_delay, MAX_TCPIP_TIMEOUT))
+                tcpip_delay += 0.25
+    finally:
+        stream.disconnect()
+        print('Exit successful, corpus dumped in %s' % (listener.dumpfile))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
